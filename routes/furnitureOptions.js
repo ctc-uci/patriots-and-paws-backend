db50c7a40a7c58596ba2fc2f6eab28d9a2722013
Rachel Villamor <ravillamor23@gmail.com>
2023-02-21 21:05:22 -0800

    Modified post request

--- routes/furnitureOptions.js
+++ routes/furnitureOptions.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { db } = require('../server/db');
 const { keysToCamel } = require('../common/utils');
 
-const router = express.Router();
+const furnitureOptionsRouter = express.Router();
 
-router.get('/', async (req, res) => {
+furnitureOptionsRouter.get('/', async (req, res) => {
   try {
     const furniture = await db.query('SELECT * FROM furniture_options');
     res.status(200).json(keysToCamel(furniture));
@@ -13,27 +13,56 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+/*
+req.body = {
+  ...,
+  furniture: [{furniture obj 1}, ...]
+}
+
+furniture = [
+  {
+    name: chair,
+    accepted: false
+  },
+  ...
+]
+nameArray = furniture.map { name:  } => name
+*/
+
+furnitureOptionsRouter.post('/', async (req, res) => {
   try {
-    const { name, accepted } = req.body;
-    const furniture = await db.query(
-      `INSERT INTO furniture_options (name, accepted)
-        VALUES (
-            $(name), $(accepted)
-        )
+    const { options, deleted } = req.body;
+    console.log(deleted);
+
+    const furnitureOptions = await db.query(
+      `INSERT INTO furniture_options(name, accepted)
+        SELECT "name", accepted
+        FROM
+            json_to_recordset($(options:json))
+        AS data("name" text, accepted boolean)
         RETURNING *;`,
       {
-        name,
-        accepted,
+        options,
       },
     );
-    res.status(200).json(keysToCamel(furniture));
+    const deletedOptions = await db.query(
+      `
+      DELETE FROM furniture_options
+      WHERE name IN $(deleted:csv)
+      RETURNING *;
+      `,
+      {
+        deleted,
+      },
+    );
+
+    res.status(200).json(keysToCamel({ options: furnitureOptions, deleted: deletedOptions }));
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-router.delete('/:name', async (req, res) => {
+furnitureOptionsRouter.delete('/:name', async (req, res) => {
   try {
     const { name } = req.params;
     const deletedFurniture = await db.query(
@@ -50,4 +79,4 @@ router.delete('/:name', async (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = furnitureOptionsRouter;
