a2c1fb428610fb98d0d7b82113719ae3369ecb89
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2023-02-27 22:43:42 -0800

    Additional Misc Functionality (#35)
    * clean up adding pictures and furniture
    
    * Create a pull trequest for branch 34-misc-func
    
    * Edited query to get a specific equation
    
    * Add furnitureCount to queries
    
    ---------
    
    Co-authored-by: Hang Cao <hmcao1@uci.edu>
    Co-authored-by: Sienna Baird <smbaird@uci.edu>
    Co-authored-by: jrock-3 <justin.liao3@gmail.com>
    Co-authored-by: Justin Liao <66648948+jrock-3@users.noreply.github.com>

--- routes/donations.js
+++ routes/donations.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const { keysToCamel, donationsQuery } = require('../common/utils');
 const { customAlphabet } = require('nanoid');
-const { keysToCamel } = require('../common/utils');
 const { db } = require('../server/db');
 
 const router = express.Router();
@@ -8,33 +8,7 @@ const router = express.Router();
 // get all donation rows
 router.get('/', async (req, res) => {
   try {
-    const allDonations = await db.query(
-      `SELECT
-      d.id, d.route_id, d.order_num, d.status,
-      d.address_street, d.address_city, d.address_unit,
-      d.address_zip, d.first_name, d.last_name, d.email,
-      d.phone_num, d.notes, d.submitted_date, relation3.pickup_date,
-      COALESCE(relation1.furniture, '{}') AS furniture,
-      COALESCE(relation2.pictures, '{}') AS pictures
-    FROM donations AS d
-    LEFT JOIN (SELECT f.donation_id,
-            array_agg(json_build_object('id', f.id, 'name', f.name)) AS furniture
-            FROM furniture AS f
-            GROUP BY f.donation_id
-          ) AS relation1
-      ON relation1.donation_id = d.id
-    LEFT JOIN (SELECT pics.donation_id,
-            array_agg(json_build_object('id', pics.id, 'image_url', pics.image_url, 'notes', pics.notes)) AS pictures
-            FROM pictures AS pics
-            GROUP BY pics.donation_id
-          ) AS relation2
-      ON relation2.donation_id = d.id
-  LEFT JOIN (
-        SELECT id AS route_id, date as pickup_date
-        FROM routes
-      ) AS relation3
-    ON relation3.route_id = d.route_id;`,
-    );
+    const allDonations = await db.query(`${donationsQuery};`);
 
     res.status(200).json(keysToCamel(allDonations));
   } catch (err) {
@@ -47,8 +21,7 @@ router.get('/:donationId', async (req, res) => {
   try {
     const { donationId } = req.params;
     const donation = await db.query(
-      `SELECT
-        id,
+      `SELECT id,
         route_id,
         order_num,
         status,
@@ -61,22 +34,26 @@ router.get('/:donationId', async (req, res) => {
         email,
         phone_num,
         notes,
-        submitted_date
-      FROM donations WHERE id = $(donationId);`,
+        submitted_date,
+        COALESCE(relation1.furniture, '{}') AS furniture,
+        COALESCE(relation2.pictures, '{}') AS pictures
+      FROM (SELECT * FROM donations WHERE id = $(donationId)) as donation
+      LEFT JOIN (SELECT f.donation_id,
+              array_agg(json_build_object('id', f.id, 'name', f.name, 'count', f.count)) AS furniture
+              FROM furniture AS f
+              GROUP BY f.donation_id
+            ) AS relation1
+        ON relation1.donation_id = donation.id
+      LEFT JOIN (SELECT pics.donation_id,
+              array_agg(json_build_object('id', pics.id, 'image_url', pics.image_url, 'notes', pics.notes)) AS pictures
+              FROM pictures AS pics
+              GROUP BY pics.donation_id
+            ) AS relation2
+        ON relation2.donation_id = donation.id;`,
       {
         donationId,
       },
     );
-    const pictureRes = await db.query(`SELECT * FROM pictures WHERE donation_id = $(donationId);`, {
-      donationId,
-    });
-    donation[0].pictures = pictureRes;
-
-    const furnitureRes = await db.query(
-      `SELECT * FROM furniture WHERE donation_id = $(donationId);`,
-      { donationId },
-    );
-    donation[0].furniture = furnitureRes;
     res.status(200).json(keysToCamel(donation));
   } catch (err) {
     res.status(500).send(err.message);
@@ -137,38 +114,26 @@ router.post('/', async (req, res) => {
       },
     );
 
-    const paddedPics = pictures.map(({ notes: note, imageUrl }) => ({
-      notes: note ?? '',
-      imageUrl,
-    }));
-    const notesArray = paddedPics.map(({ notes: note }) => note);
-    const urlArray = paddedPics.map(({ imageUrl: url }) => url);
     const donationId = donation[0].id;
 
     const picturesRes = await db.query(
-      `INSERT INTO pictures(donation_id, image_url, notes) 
-      SELECT $(donationId) donation_id, notes, imageURL FROM
-          unnest(
-            $(notesArray),
-            $(urlArray)
-          ) AS data(notes, imageURL)
-        RETURNING *;`,
-      { donationId, notesArray, urlArray },
+      `INSERT INTO pictures(donation_id, image_url, notes)
+      SELECT $(donationId), "imageUrl", notes
+      FROM
+          json_to_recordset($(pictures:json))
+      AS data("imageUrl" text, notes text)
+      RETURNING *;`,
+      { donationId, pictures },
     );
 
-    const nameArray = furniture.map(({ name }) => name);
-    const countArray = furniture.map(({ count }) => count);
-
     const furnitureRes = await db.query(
-      `INSERT INTO furniture(donation_id, name, count) 
-      SELECT $(donationId) donation_id, name, count
+      `INSERT INTO furniture(donation_id, name, count)
+      SELECT $(donationId), name, count
       FROM
-          unnest(
-            $(nameArray),
-            $(countArray)
-          ) AS data(name, count)
+          json_to_recordset($(furniture:json))
+      AS data(name text, count integer)
       RETURNING *;`,
-      { donationId, nameArray, countArray },
+      { donationId, furniture },
     );
 
     donation[0].pictures = picturesRes;
@@ -199,6 +164,7 @@ router.post('/verify', async (req, res) => {
 
 // update info for a specific donation
 router.put('/:donationId', async (req, res) => {
+  // add update furniture & pictures (can do this in multiple queries, not super inefficient as we won't be updating more than a certain number of rows at a time)
   try {
     const { donationId } = req.params;
     const {
